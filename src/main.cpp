c8baec8514a2acdc7af1bfba53cd31b21fce7608
William Herd <williamherd25@gmail.com>
2024-01-01 16:52:40 +0700

    Import

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <UbxGps.h>
+
+
+// put function declarations here:
+// int myFunction(int, int);
+
+int x = 10;
+int temp;
+
+void setup() {
+  // put your setup code here, to run once:
+  temp = 10;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+temp*=1.00005;
+}
+
+// put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
